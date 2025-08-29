dbf327b36fd59525ba704410dc601103a8c6ed02
OngTK <dhdxorud@gmail.com>
2025-08-29 15:33:04 +0900

    250829_1452 RT기능구현중

--- src/main/java/five_minutes/controller/RtController.java
+++ src/main/java/five_minutes/controller/RtController.java
@@ -1,8 +1,11 @@
 package five_minutes.controller;
 
+import five_minutes.model.dto.RtDto;
 import five_minutes.service.RtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 // Info =========================
 // RoleTemplate Controller
@@ -10,11 +13,38 @@ import org.springframework.web.bind.annotation.RestController;
 // Google sheet > 21.테이블 > 2-1
 // 작성자 : OngTK
 
+// TODO 옹태경
+
 @RestController             // 컨트롤러 컴포넌트
 @RequiredArgsConstructor    // 의존성 주입
 public class RtController {  // class start
 
     private final RtService rtService;
+
+    // [ RT-01 ] 역할 템플릿 생성 createRT()
+    public int createRT(){
+        return  0;
+    }// [ RT-01 ]  func end
+
+    // [ RT-02 ] 역할 템플릿 전체 조회 getRT()
+    public List<RtDto> getRT(){
+        return null;
+    }// [ RT-02 ]  func end
+
+    // [ RT-03 ] 역할 템플릿 개별 조회 getIndiRT()
+    public RtDto getIndiRT(){
+        return null;
+    }// [ RT-03 ]  func end
+
+    // [ RT-04 ] 역할 템플릿 수정 updateRT()
+    public int updateRT(){
+        return  0;
+    }// [ RT-04 ]  func end
+
+    // [ RT-05 ] 역할 템플릿 삭제(비활성화) deleteRT()
+    public int deleteRT(){
+        return  0;
+    }// [ RT-05 ]  func end
 
 
 
--- src/main/java/five_minutes/model/dao/RtDao.java
+++ src/main/java/five_minutes/model/dao/RtDao.java
@@ -1,6 +1,11 @@
 package five_minutes.model.dao;
 
+import five_minutes.model.dto.RtDto;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
 
 // Info =========================
 // RoleTemplate Dao
@@ -10,5 +15,63 @@ import org.springframework.stereotype.Repository;
 
 @Repository     // Dao 어노테이션
 public class RtDao extends Dao {
+
+    // [ RT-01 ] 역할 템플릿 생성 createRT()
+    public int createRT(RtDto rtDto) {
+        try {
+            String sql = "insert into RoleTemplete(rtName, rtDescription, bnNo) values (?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1,rtDto.getRtName());
+            ps.setString(2, rtDto.getRtDescription());
+            ps.setInt(3,rtDto.getBnNo());
+            int count = ps.executeUpdate();
+            if (count == 1 ){
+
+            }
+        } catch (Exception e) {
+            System.out.println("RtDao.createRT " + e);
+        }
+        return 0;
+    }// [ RT-01 ]  func end
+
+    // [ RT-02 ] 역할 템플릿 전체 조회 getRT()
+    public List<RtDto> getRT() {
+        try {
+            String sql = "";
+        } catch (Exception e) {
+            System.out.println("RtDao.getRT " + e);
+        }
+        return null;
+    }// [ RT-02 ]  func end
+
+    // [ RT-03 ] 역할 템플릿 개별 조회 getIndiRT()
+    public RtDto getIndiRT() {
+        try {
+            String sql = "";
+        } catch (Exception e) {
+            System.out.println("RtDao.getIndiRT " +e );
+        }
+        return null;
+    }// [ RT-03 ]  func end
+
+    // [ RT-04 ] 역할 템플릿 수정 updateRT()
+    public int updateRT() {
+        try {
+            String sql = "";
+        } catch (Exception e) {
+            System.out.println("RtDao.updateRT " +e);
+        }
+        return 0;
+    }// [ RT-04 ]  func end
+
+    // [ RT-05 ] 역할 템플릿 삭제(비활성화) deleteRT()
+    public int deleteRT() {
+        try {
+            String sql = "";
+        } catch (Exception e) {
+            System.out.println("RtDao.deleteRT " +e);
+        }
+        return 0;
+    }// [ RT-05 ]  func end
 
 }   // class end
