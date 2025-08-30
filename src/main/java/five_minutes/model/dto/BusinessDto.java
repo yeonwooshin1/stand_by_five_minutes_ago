66d3de7e2960c585684e96b32c286b827550b284
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-08-31 04:52:06 +0900

    250831 - BusinessDto 주석 추가

--- src/main/java/five_minutes/model/dto/BusinessDto.java
+++ src/main/java/five_minutes/model/dto/BusinessDto.java
@@ -6,17 +6,16 @@ import lombok.NoArgsConstructor;
 
 @AllArgsConstructor @NoArgsConstructor @Data    // lombok
 public class BusinessDto { // class start
-
-    private String bnNo;
-    private String bnName;
-    private String managerName;
-    private String managerPhone;
-    private String bnDocuImg ;
-    private String bnType;
-    private String bnItem;
-    private int userNo;
-    private String createDate;
-    private String updateDate;
-    private String bnStatus;
+    private String bnNo;                // 사업자등록번호
+    private String bnName;              // 기업명
+    private String managerName;         // 담당자명
+    private String managerPhone;        // 담당자번호
+    private String bnDocuImg ;          // 사업자이미지
+    private String bnType;              // 업태
+    private String bnItem;              // 종목
+    private int userNo;                 // 상태
+    private String createDate;          // 기업등록일
+    private String updateDate;          // 기업정보수정일
+    private String bnStatus;            // 사용자번호
 
 }   // class end
