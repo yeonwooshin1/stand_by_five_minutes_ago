28180c30b985638affe6b6fe2342c598d57c2b0a
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-01 17:37:45 +0900

    250901 - 전체 취합한 거 커밋푸시 동진님 오류 해결 후 main에서 풀 한 후 다시 푸시

--- src/main/java/five_minutes/AppStart.java
+++ src/main/java/five_minutes/AppStart.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication  // 스프링부트 어노테이션
 public class AppStart { // class start
     public static void main(String[] args) {    // main start
-
         SpringApplication.run(AppStart.class);
-
     }   // main end
 }   // class end
