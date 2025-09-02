4b3672001ee45abc95e612cb9258bf07a3e42fe7
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-03 04:56:39 +0900

    250902 - AppStart 비동기 어노테이션 추가 EmailService에 비동기 어노테이션 추가 이메일 서비스에 필요함

--- src/main/java/five_minutes/AppStart.java
+++ src/main/java/five_minutes/AppStart.java
@@ -2,8 +2,10 @@ package five_minutes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication  // 스프링부트 어노테이션
+@EnableAsync // 비동기 사용 활성화
 public class AppStart { // class start
     public static void main(String[] args) {    // main start
         SpringApplication.run(AppStart.class);
