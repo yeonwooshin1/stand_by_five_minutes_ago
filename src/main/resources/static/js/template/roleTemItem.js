f94032d08df25bbd2aaf90bbde8ae358302ad481
OngTK <dhdxorud@gmail.com>
2025-09-02 15:20:14 +0900

    250902_1519 roleTem.js try-catch 수정
    fetch를 진행하는 모든 func 의 try-catch 삽입

--- src/main/resources/static/js/template/roleTemItem.js
+++ src/main/resources/static/js/template/roleTemItem.js
@@ -4,7 +4,7 @@ console.log("roleTemItem js func")
 const params = new URL(location.href).searchParams;
 const rtNo = params.get("rtNo")
 console.log(rtNo)
-const rtInfo = {rtNo : rtNo, rtDescription : ""}
+const rtInfo = { rtNo: rtNo, rtDescription: "" }
 
 // [ 상세 역할 템플릿 생성 모달 내 Summer Note 연동 ]
 $(document).ready(function () {
@@ -34,10 +34,15 @@ const getIndiRT = async () => {
 
     // [1.1] 표시할 영역 가져오기
     // 상단 타이틀 옆에 대분류 명을 표시
-    const rtName = document.querySelector(".title1 span")
-   
-    // 세부 템플릿 미리보기/수정하기 내에 대분류 탬플릿도 함께 표시
+    const rtNameBox = document.querySelector(".titleBox span")
+    const rtName01 = document.querySelector(".rtName01");
+    const rtDescription01 = document.querySelector(".rtDescription01");
+    const rtName02 = document.querySelector(".rtName02");
+    const rtDescription02 = document.querySelector(".rtDescription02");
+    const rtName03 = document.querySelector(".rtName03");
+    const rtDescription03 = document.querySelector(".rtDescription03");
 
+    // 세부 템플릿 미리보기/수정하기 내에 대분류 탬플릿도 함께 표시
 
     // [1.2] Fetch
     const r = await fetch(`/roleTem/indi?rtNo=${rtNo}`)
@@ -45,25 +50,33 @@ const getIndiRT = async () => {
     console.log(d)
 
     // [3.3] 화면에 표시
-    rtName.innerHTML = `- ${d.rtName}`
-    rtInfo.rtDescription = d.rtDescription;
+    rtNameBox.innerHTML = `- ${d.rtName}`
+    rtName01.value = d.rtName;
+    rtDescription01.innerHTML = d.rtDescription;
+    rtName02.value = d.rtName;
+    rtDescription02.innerHTML = d.rtDescription;
+    rtName03.value = d.rtName;
+    rtDescription03.innerHTML = d.rtDescription;
 } // func end
-
-getIndiRT().then(() => {
-    console.log(rtInfo.rtNo);
-    console.log(rtInfo.rtDescription);
-});
+getIndiRT()
 
 // [상세 역할 템플릿 관련 func] =====================================
 
 // [RTI-01] 상세역할템플릿 생성 
-const createRTItem = () => {
+const createRTItem = async () => {
     // [1.1] 입력할 데이터 가져오기
+    const rtiName = document.querySelector("#rtiNameInput")
+    const rtiDescription = document.querySelector("#rtiDescription")
     
     // [1.2] Fetch
-
+    const obj = {rtNo, rtiName, rtiDescription}
+    const opt = {method : "POST",
+        headers : {"Content-Type":"application/json"},
+        body : JSON.stringify(obj)
+    }
+    const r = await fetch()
     // [1.3] 결과
-}
+} // func end
 
 // [RTI-02] 상세역할템플릿 전체 조회 getRTItem()
 
