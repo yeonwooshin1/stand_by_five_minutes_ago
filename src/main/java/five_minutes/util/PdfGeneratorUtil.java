9d335560824f62520769426e194f35e6443dd2be
kafella92 <choidj10@gmail.com>
2025-09-03 14:02:11 +0900

    250903 라이브러리 추가 그래프호퍼 ( 경로 탐색, 이동 거리와 시간 계산 )

--- src/main/java/five_minutes/util/PdfGeneratorUtil.java
+++ src/main/java/five_minutes/util/PdfGeneratorUtil.java
@@ -9,36 +9,37 @@ import com.lowagie.text.pdf.PdfWriter;
 
 import java.io.FileOutputStream;
 
-public class PdfGeneratorUtil{
-    public static void createPdf () {
+public class PdfGeneratorUtil {
+    public static void main(String[] args) {
         // 1. Document 객체를 생성합니다.
         Document document = new Document();
         // 2. 파일명을 정의합니다.
         String outputPath = "HelloWorld.pdf";
-        try{
+        try {
             // 3. PdfWriter로 Document를 파일 스트림에 연결합니다
-            PdfWriter.getInstance(document , new FileOutputStream(outputPath));
+            PdfWriter.getInstance(document, new FileOutputStream(outputPath));
             // 4. Document를 엽니다.
             document.open();
             // 5. 영문 폰트는 기본으로 설정합니다. (5번 따라하면 커스텀 가능)
             // Font defaultFont = new Font(Font.HELVETICA, 12 ,Font.NORMAL);
             // 6. 한글 폰트를 쓰고 싶다면, 폰트가 있는 경로에 폰트 파일을 배치
             // * noto sans kr가 한글 영문을 모두 지원하므로, 하나로 통일합니다.
-            BaseFont baseFont = BaseFont.createFont("/font/NotoSansKR-VariableFont_wght.ttf" , BaseFont.IDENTITY_H , BaseFont.EMBEDDED);
-            Font font = new Font(baseFont , 12);
+            BaseFont baseFont = BaseFont.createFont("/font/NotoSansKR-VariableFont_wght.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont, 12);
             // 7. 내용 추가
-            document.add(new Paragraph("Hello, OpenPDF!" , font));
-            document.add(new Paragraph("안녕하세요. OpenPDF를 사용한 한글 PDF 생성 예제입니다." , font));
+            document.add(new Paragraph("Hello, OpenPDF!", font));
+            document.add(new Paragraph("안녕하세요. OpenPDF를 사용한 한글 PDF 생성 예제입니다.", font));
             System.out.println("'" + outputPath + "' 파일이 성공적으로 생성되었습니다.");
 
         } catch (Exception e) {
             System.out.println(e);
         } finally {
             // 8. Document 닫기
-            if(document.isOpen()) {
+            if (document.isOpen()) {
                 document.close();
             }
         }
+
     }
 
 }
