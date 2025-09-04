07a9595580692ed51586734fce0acb574b65513f
kafella92 <choidj10@gmail.com>
2025-09-04 09:38:30 +0900

    250904 스웨거, 발리데이션 라이브러리 CTItemDto 작업 중

--- src/main/java/five_minutes/model/dto/CTItemDto.java
+++ src/main/java/five_minutes/model/dto/CTItemDto.java
@@ -1,5 +1,8 @@
 package five_minutes.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,13 @@ import lombok.NoArgsConstructor;
 public class CTItemDto {
     // CheckTemplateItem
     // DB 멤버변수
+    @Schema(description = "상세 체크리스트 템플릿 번호", example = "5000001" , minimum = "5000001" , maximum = "5999999")
+    @Positive(message = "상세 체크리스트 템플릿 번호는 양수만 가능합니다.")
     private int ctiNo;
+    @Schema(description = "상세 체크리스트 템플릿 제목", example = "현장도착")
+    @NotBlank(message = "상세 체크리스트 템플릿 제목은 필수 입력 항목입니다.")
     private String ctiTitle;
+    @Schema(description = "상세 도움말", example = "현장 도착 후 출근 업무를 위한 세부 작업 1 수행")
     private String ctiHelpText;
     private int ctiStatus;
     private String createDate;
--- src/main/java/five_minutes/model/dto/CTemDto.java
+++ src/main/java/five_minutes/model/dto/CTemDto.java
@@ -19,7 +19,7 @@ public class CTemDto {
     // DB 멤버변수
     // 스웨거는 자동 스트링 타입. example에는 int값도 따옴표 넣기
     @Schema(description = "체크리스트 템플릿 번호", example = "4000001" , minimum = "4000001" , maximum = "4999999")
-    @Positive
+    @Positive(message = "체크리스트 템플릿 번호는 양수만 가능합니다.")
     private int ctNo;
     @Schema(description = "체크리스트 템플릿 이름", example = "출근확인")
     @NotBlank(message = "체크리스트 템플릿 이름은 필수 입력 항목입니다.")
@@ -27,7 +27,7 @@ public class CTemDto {
     @Schema(description = "체크리스트 템플릿 설명", example = "출근 최소 5분 전까지는 출장장소에 출근 여부 확인")
     private String ctDescription;
     @Schema(description = "상태(1은 활성화, 0은 비활성화)", example = "1" , defaultValue = "1")
-    @PositiveOrZero
+    @PositiveOrZero(message = "상태는 1(활성화) 또는 0(비활성화)여야 합니다. ")
     private int ctStatus;
     @Schema(description = "생성일", example = "2025-08-25 12:30:40")
     private String createDate;
