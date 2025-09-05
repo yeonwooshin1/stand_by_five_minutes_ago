0c44f62844661d72ac988d6b046156ed5f6ec820
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-05 16:40:35 +0900

    250905 - ChkItemLookupDto 추가 chkItem 부분 FK 하는 데이터 가져오는 DTO 추가

---
+++ src/main/java/five_minutes/model/dto/ChkItemLookupDto.java
@@ -0,0 +1,15 @@
+package five_minutes.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data    // lombok
+public class ChkItemLookupDto {     // class start
+
+    private int pjChkItemNo;           // -- 체크리스트 아이템 번호 PK
+    private String pjChklTitle;        // -- 체크리스트 제목
+
+}   // class end
---
+++ src/main/java/five_minutes/model/dto/ReturnProjectPerformDto.java
@@ -0,0 +1,9 @@
+package five_minutes.model.dto;
+
+import java.util.Map;
+
+public class ReturnProjectPerformDto {  // class start
+
+    private Map<String, Long> createdMap;   // 성공한 값 저장
+}   // class end
+
