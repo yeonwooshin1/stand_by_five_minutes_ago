4f522338f6eeb1446b891a3d2918c25470706818
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-05 18:12:17 +0900

    250905 - 집 가기전 전체 취합

--- src/main/java/five_minutes/AppStart.java
+++ src/main/java/five_minutes/AppStart.java
@@ -8,6 +8,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync // 비동기 사용 활성화
 public class AppStart { // class start
     public static void main(String[] args) {    // main start
+
         SpringApplication.run(AppStart.class);
+
     }   // main end
 }   // class end
