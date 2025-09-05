379d175ce40d67129a0a95fb83e7b0b7f18640a4
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-09-05 16:41:11 +0900

    250905 - ProjectPerformController 추가

---
+++ src/main/java/five_minutes/controller/ProjectPerformController.java
@@ -0,0 +1,38 @@
+package five_minutes.controller;
+
+import five_minutes.model.dto.ProjectPerformDto;
+import five_minutes.service.ProjectPerformService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController             // 컨트롤러 컴포넌트
+@RequiredArgsConstructor    // 의존성 주입
+@RequestMapping("/projectPerform")
+public class ProjectPerformController { // class start
+
+    // 서비스 호출
+    private final ProjectPerformService projectPerformService;
+
+    // pjPerform 목록 가져오기
+    @GetMapping
+    public List<ProjectPerformDto> list(@RequestParam int pjNo) {   // class start
+        return projectPerformService.findByProject(pjNo);
+    }   // func end
+
+    // FK 하는 값들 필요한 것만 가져오기
+    @GetMapping("/lookup")
+    public Map<String, Object> lookups(@RequestParam int pjNo) {
+        return Map.of("roles", projectPerformService.findRolesLookup(pjNo),
+                "chkItems", projectPerformService.findChkItemsLookup(pjNo)
+        );
+    }   // func end
+
+//    @PostMapping("/save")
+//    public ReturnProjectPerformDto save(@RequestParam Long pjNo, @RequestBody SaveProjectPerformDto req) {
+//
+//    }
+
+}   // class end
